8f6d35c69fafbb0826440cfb23bb753bb6bd82aa
Fatih Akgöze <f.akgoze@gmail.com>
2018-05-06 19:10:26 +0300

    Added firebase project settings.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "<API_KEY>",
+    authDomain: "<AUTH_DOMAIN>",
+    databaseURL: "<DATABASE_URL>",
+    projectId: "<PROJET_ID>",
+    storageBucket: "<STORAGE_BUCKET>",
+    messagingSenderId: "<MESSAGGING_ID>"
+  }
 };
