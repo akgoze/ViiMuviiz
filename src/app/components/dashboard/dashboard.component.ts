7a90b9b36c7a25bab1ccf504da6f1f2a18cf25eb
Fatih Akgöze <f.akgoze@gmail.com>
2018-05-07 23:47:58 +0300

    Mapping in dashboard.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -3,16 +3,31 @@ import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument 
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
 
+interface Movie {
+  movie_id: string;
+  movie_name: string;
+  movie_type: string;
+  movie_poster: string;
+  movie_rank: number;
+  id?: string;
+}
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+    
+  moviesCollection : AngularFirestoreCollection<Movie>;
+  movies: Observable<Movie[]>;
 
-  constructor() { }
-
+  constructor(private afs: AngularFirestore) { }
+  
   ngOnInit() {
-  }
 
+    this.moviesCollection = this.afs.collection('movies')
+    this.movies = this.moviesCollection.valueChanges()
+    
+  }
 }
