eb0a3000041c65bd7d530d3e82b7707d4869ed79
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2018-05-22 11:42:44 +0300

    added viewDetail movie

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -8,9 +8,24 @@ import { HttpClientModule } from '@angular/common/http';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
+
 export class SearchComponent implements OnInit {
   movies;
-  
+  movieDetail;
+  movieItem=[];
+  movieDetails = {
+    title: "",
+    plot: "",
+    poster: "",
+    actors: "",
+    awards: "",
+    country: "",
+    director: "",
+    genre: "",
+    language: "",
+    rated: ""
+  };
+
   constructor(private movieService:MovieService) { }
   
   ngOnInit() {
@@ -19,10 +34,28 @@ export class SearchComponent implements OnInit {
     this.movieService.GetMoviesFromApi(title)
       .subscribe(response => {
         this.movies = response.Search;
+        console.log(response);
       }
     );
       
   }
 
-
+  
+  viewDetail(imdbID:string) {
+    this.movieService.GetMovieDetailFromApi(imdbID)
+      .subscribe(response => {
+        this.movieDetail = response;
+        console.log(response);
+        this.movieDetails.title = this.movieDetail.Title;
+        this.movieDetails.plot = this.movieDetail.Plot;
+        this.movieDetails.poster = this.movieDetail.Poster;
+        this.movieDetails.actors = this.movieDetail.Actors; 
+        this.movieDetails.awards = this.movieDetail.Awards;
+        this.movieDetails.country = this.movieDetail.Country;
+        this.movieDetails.director = this.movieDetail.Director;
+        this.movieDetails.genre = this.movieDetail.Genre;
+        this.movieDetails.language = this.movieDetail.Language;
+        this.movieDetails.rated = this.movieDetail.Rated;
+      });
+  }
 }
