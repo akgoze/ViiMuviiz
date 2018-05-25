c8a7478c65a565dbf6fa434303672e654fe6b48d
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2018-05-25 18:02:13 +0300

    Added movie detail compometm

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -3,12 +3,18 @@ import { HttpClient } from '@angular/common/http';
  
 @Injectable() 
 export class MovieService { 
- 
+  
+  apikey: string = '9e0b89ad';
+
   constructor(private http:HttpClient) { } 
- 
+  
  
   GetMoviesFromApi(title:string) { 
-     return this.http.get(`http://www.omdbapi.com/?s=${title}&apikey=9e0b89ad`); 
+     return this.http.get(`http://www.omdbapi.com/?s=${title}&apikey=${this.apikey}`); 
   } 
- 
+  
+  GetMovieDetailFromApi(imdbID:string) {
+    return this.http.get(`http://www.omdbapi.com/?i=${imdbID}&apikey=${this.apikey}`)
+  }
+
 } 
