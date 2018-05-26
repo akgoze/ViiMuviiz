d193213518c41a6bb9c8763ac6af41cbb76d88a0
Fatih Akgöze <f.akgoze@gmail.com>
2018-05-26 20:26:17 +0300

    Added AddMovie method.

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,7 +1,9 @@
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { Title } from '@angular/platform-browser';
 import { MovieService } from './../../services/movie.service';
 import { Component, OnInit } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'; 
+import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-search',
@@ -26,7 +28,9 @@ export class SearchComponent implements OnInit {
     rated: ""
   };
 
-  constructor(private movieService:MovieService) { }
+ 
+
+  constructor(private movieService:MovieService, private afs:AngularFirestore) { }
   
   ngOnInit() {
   }
@@ -37,7 +41,6 @@ export class SearchComponent implements OnInit {
         console.log(response);
       }
     );
-      
   }
 
   
@@ -48,14 +51,42 @@ export class SearchComponent implements OnInit {
         console.log(response);
         this.movieDetails.title = this.movieDetail.Title;
         this.movieDetails.plot = this.movieDetail.Plot;
-        this.movieDetails.poster = this.movieDetail.Poster;
         this.movieDetails.actors = this.movieDetail.Actors; 
         this.movieDetails.awards = this.movieDetail.Awards;
-        this.movieDetails.country = this.movieDetail.Country;
         this.movieDetails.director = this.movieDetail.Director;
         this.movieDetails.genre = this.movieDetail.Genre;
-        this.movieDetails.language = this.movieDetail.Language;
         this.movieDetails.rated = this.movieDetail.Rated;
       });
+  }
+
+
+  AddMovie(movieId:HTMLInputElement, movieTitle: HTMLInputElement, moviePoster: HTMLInputElement) {
+    this.movieService.GetMovieDetailFromApi(movieId.value)
+    .subscribe(response => {
+      
+      const createdAt:Date = new Date();
+      const id = this.afs.createId();
+
+        this.movieDetail = response;
+
+        console.log(this.movieDetail.Ratings[0].Value);
+        
+
+        const newMovie = {
+          name: movieTitle.value,
+          id: movieId.value,
+          poster: moviePoster.value,
+          plot: this.movieDetail.Plot,
+          actor: this.movieDetail.Actors,
+          ratings: this.movieDetail.Ratings,
+          createdAt: createdAt
+        }
+        this.afs.firestore.doc('movies/'+id).set(newMovie).then(function () {
+          console.log("ADDED NEW MOVIE");
+        }).catch(function () {
+          console.log("ERROR");
+        });
+      
+    });
   }
 }
